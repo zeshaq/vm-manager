c25e35b757ad8b0700b96f01b9e204aeca3e0de4
ze <ze@zes-MacBook-Pro.local>
2025-12-06 01:09:22 +0600

    25

--- views/creation.py
+++ views/creation.py
@@ -3,11 +3,11 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 creation_bp = Blueprint('creation', __name__)
 
-def generate_vm_xml(name, memory_mb, vcpus, disk_path, iso_path, host_cpu=False):
+def generate_vm_xml(name, memory_mb, vcpus, host_cpu=False):
+    # Convert MB to KiB
     memory_kib = int(memory_mb) * 1024
     
     # CPU Model Configuration
-    # host-passthrough: Best performance, exposes host capabilities (AVX, nested virt) to VM
     cpu_xml = ""
     if host_cpu:
         cpu_xml = "<cpu mode='host-passthrough' check='none'/>"
@@ -22,25 +22,19 @@ def generate_vm_xml(name, memory_mb, vcpus, disk_path, iso_path, host_cpu=False)
         <type arch='x86_64' machine='pc-q35-6.2'>hvm</type>
         <boot dev='hd'/>
         <boot dev='cdrom'/>
+        <boot dev='network'/>
       </os>
       <features><acpi/><apic/></features>
       <devices>
         <emulator>/usr/bin/qemu-system-x86_64</emulator>
-        <disk type='file' device='disk'>
-          <driver name='qemu' type='qcow2'/>
-          <source file='{disk_path}'/>
-          <target dev='vda' bus='virtio'/>
-        </disk>
-        <disk type='file' device='cdrom'>
-          <driver name='qemu' type='raw'/>
-          <source file='{iso_path}'/>
-          <target dev='sda' bus='sata'/>
-          <readonly/>
-        </disk>
+        
+        <!-- Network Interface (Default NAT) -->
         <interface type='network'>
           <source network='default'/>
           <model type='virtio'/>
         </interface>
+        
+        <!-- Graphics -->
         <graphics type='vnc' port='-1' autoport='yes' listen='0.0.0.0'/>
       </devices>
     </domain>
@@ -51,15 +45,14 @@ def create_vm():
     if request.method == 'POST':
         try:
             name = request.form['name']
-            ram = request.form['ram']
+            ram = request.form['ram'] # Value is in MB
             cpu = request.form['cpu']
-            disk = request.form['disk_path']
-            iso = request.form['iso_path']
             
-            # Checkbox returns 'on' if checked, None if not
+            # CPU Passthrough Checkbox
             use_host_cpu = request.form.get('host_cpu') == 'on'
 
-            xml_config = generate_vm_xml(name, ram, cpu, disk, iso, use_host_cpu)
+            # Generate XML without disks
+            xml_config = generate_vm_xml(name, ram, cpu, use_host_cpu)
 
             conn = libvirt.open('qemu:///system')
             if conn:
@@ -67,7 +60,7 @@ def create_vm():
                 conn.close()
                 return redirect(url_for('listing.list_vms'))
                 
-        except libvirt.libvirtError as e:
+        except Exception as e:
             return f"<h1>Error Creating VM</h1><p>{e}</p><a href='/create'>Try Again</a>"
 
     return render_template('create.html')
